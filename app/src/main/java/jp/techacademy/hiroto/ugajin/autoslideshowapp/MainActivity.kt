4a0080a4abc7ca20f785c633fbaac0741ddb3178
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-05-21 07:20:07 +0900

    5/21:往復方向完成

--- app/src/main/java/jp/techacademy/hiroto/ugajin/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/autoslideshowapp/MainActivity.kt
@@ -20,8 +20,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     var num: Int = 0
 
-//    private var currentPosition: Int = -1
-
     private val PERMISSIONS_REQUEST_CODE = 100
 
     // APIレベルによって許可が必要なパーミッションを切り替える
@@ -35,7 +33,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val view = binding.root
         setContentView(view)
 
-        binding.button1.setOnClickListener(this)
+//        binding.buttonGo.setOnClickListener (this)
+        binding.buttonGo.setOnClickListener(buttonGoClickListener)
+        binding.buttonBack.setOnClickListener(buttonBackClickListener)
 
         // パーミッションの許可状態を確認する
         if (checkSelfPermission(readImagesPermission) == PackageManager.PERMISSION_GRANTED) {
@@ -80,9 +80,54 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
                 if (currentPosition < this.num - 1) { currentPosition ++
                 } else if (currentPosition == this.num - 1) { currentPosition = 0
-            }
+                }
            }
         }
+
+        if (cursor != null) {
+            cursor.close()
+        }
+    }
+
+    private fun displayPreviousImage() {
+
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目（null = 全項目）
+            null, // フィルタ条件（null = フィルタなし）
+            null, // フィルタ用パラメータ
+            null // ソート (nullソートなし）
+        )
+        if (cursor != null) {
+            this.num = cursor.count
+        }
+
+        if (cursor != null && currentPosition > 0) {
+            if (cursor.moveToPosition(currentPosition - 1) != null) {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                binding.imageView.setImageURI(imageUri)
+                currentPosition --}
+
+                } else if (currentPosition == 0) {
+            if (cursor != null) {
+                if (cursor.moveToPosition(0) != null) {
+                    // indexからIDを取得し、そのIDから画像のURIを取得する
+                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                    val id = cursor.getLong(fieldIndex)
+                    val imageUri =
+                        ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                    binding.imageView.setImageURI(imageUri)
+                    currentPosition = num
+                }
+            }
+            }
 
         if (cursor != null) {
             cursor.close()
@@ -92,6 +137,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         displayNextImage()
     }
+
+    private val buttonGoClickListener = View.OnClickListener {
+        displayNextImage()
+    }
+
+    private val buttonBackClickListener = View.OnClickListener {
+        displayPreviousImage()
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
