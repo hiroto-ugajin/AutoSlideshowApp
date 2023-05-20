fb8c10eebeeab980abb1eafce400308134f1f165
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-05-20 16:19:03 +0900

    5/20:順方向ほぼ完成

--- app/src/main/java/jp/techacademy/hiroto/ugajin/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/autoslideshowapp/MainActivity.kt
@@ -33,14 +33,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val view = binding.root
         setContentView(view)
 
-//        val resolver = contentResolver
-//        cursor = resolver.query(
-//            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-//            null, // 項目（null = 全項目）
-//            null, // フィルタ条件（null = フィルタなし）
-//            null, // フィルタ用パラメータ
-//            null // ソート (nullソートなし）
-//        )!!
+        binding.button1.setOnClickListener(this)
 
         // パーミッションの許可状態を確認する
         if (checkSelfPermission(readImagesPermission) == PackageManager.PERMISSION_GRANTED) {
@@ -57,7 +50,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private var currentPosition: Int = -1
+    private var currentPosition: Int = 0
 
     private fun displayNextImage() {
 
@@ -73,7 +66,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         if (cursor != null) {
             if (
     //            cursor != null &&
-                cursor.moveToNext() != null) {
+                cursor.moveToPosition(currentPosition)
+//                cursor.moveToNext()
+                != null
+            )
+            {
                 // indexからIDを取得し、そのIDから画像のURIを取得する
                 val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
                 val id = cursor.getLong(fieldIndex)
@@ -81,19 +78,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
                 binding.imageView.setImageURI(imageUri)
-                currentPosition = cursor.position
+                currentPosition ++
+//                = cursor.position
             } else {
+
                 // カーソルの最後に達した場合、最初に戻る
-                if (cursor.moveToFirst() != null) {
-                    cursor.moveToFirst()
+                if (cursor.moveToPosition(0) != null) {
+                    cursor.moveToPosition(0)
                 }
-                currentPosition = -1
+                currentPosition = 1
             }
+        }
+
+        if (cursor != null) {
+            cursor.close()
         }
     }
 
     override fun onClick(v: View) {
         displayNextImage()
+        Log.d("UI_PARTS", "進むボタンがタップされました。")
     }
 
     override fun onRequestPermissionsResult(
